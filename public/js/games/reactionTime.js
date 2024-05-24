916a456044eae2fe7aec20100e8bbb4a2d2f2d1b
abder <ismail.benlaidi@ynov.com>
2024-05-24 16:40:04 +0200

    nodemodules deleted

---
+++ public/js/games/reactionTime.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('reactionTimeCanvas');
+const context = canvas.getContext('2d');
+const startButton = document.getElementById('startButton');
+const reactionTime = document.getElementById('reactionTime');
+const header = document.getElementById('header');
+const resetButton = document.getElementById('resetButton');
