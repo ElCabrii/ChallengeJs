f48999b6319d53df130d87f996587397f3369431
abder <ismail.benlaidi@ynov.com>
2024-05-31 14:32:51 +0200

    reactionTime initiated

--- public/js/games/reactionTime.js
+++ public/js/games/reactionTime.js
@@ -1,6 +1,21 @@
-const canvas = document.getElementById('reactionTimeCanvas');
-const context = canvas.getContext('2d');
+const gameArea = document.getElementById('gameArea');
 const startButton = document.getElementById('startButton');
-const reactionTime = document.getElementById('reactionTime');
-const header = document.getElementById('header');
-const resetButton = document.getElementById('resetButton');
+let startTime;
+let endTime;
+
+function startGame() {
+  gameArea.innerHTML = '';
+  startButton.style.display = 'none';
+  setTimeout(() => {
+    gameArea.style.backgroundColor = 'red';
+    startTime = Date.now();
+  }, Math.random() * 2000 + 1000);
+}
+
+function endGame() {
+  endTime = Date.now();
+  const reactionTime = endTime - startTime;
+  gameArea.innerHTML = `Your reaction time is ${reactionTime}ms`;
+  gameArea.style.backgroundColor = 'white';
+  startButton.style.display = 'block';
+}
