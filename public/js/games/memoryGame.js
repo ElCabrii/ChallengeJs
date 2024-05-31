594cfb3f2c75eee344c5a9e822ab4a016b5c56af
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-05-31 16:40:35 +0200

    memory game now working

--- public/js/games/memoryGame.js
+++ public/js/games/memoryGame.js
@@ -1,9 +1,7 @@
-const score = document.getElementById('score');
+const errors = document.getElementById('errors');
 const game = document.getElementById('game');
-const startButton = document.getElementById('startButton');
 const resetButton = document.getElementById('resetButton');
 
-let errors = 0;
 let cardList = [
     'Enzo',
     'Seb',
@@ -24,6 +22,17 @@ window.onload = () => {
     startGame();
 }
 
+resetButton.onclick = () => {
+    cardSet = [];
+    board = [];
+    card1 = null;
+    card2 = null;
+    game.innerHTML = "";
+    errors.innerText = 0;
+    shuffleCards();
+    startGame();
+}
+
 function shuffleCards() {
     cardSet = cardList.concat(cardList).concat(cardList).concat(cardList);
     cardSet.sort(() => Math.random() - 0.5);
@@ -36,8 +45,9 @@ function startGame() {
             let cardImg = cardSet.pop();
             row.push(cardImg);
             let card = document.createElement("img");
-            card.id = (c*10+r).toString();
-            card.src = "/img/memoryGame/" + cardImg + ".jpg";
+            card.style.display = "none";
+            card.id = r.toString() + "-" + c.toString();
+            card.src = "/img/memoryGame/" + cardImg + ".png";
             card.classList.add("card");
             card.addEventListener("click", selectCard);
             game.append(card);
@@ -50,8 +60,9 @@ function startGame() {
 function hideCards() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
-            let card = document.getElementById((c*10+r).toString());
+            let card = document.getElementById(r.toString() + "-" + c.toString());
             card.src = "/img/memoryGame/back.png";
+            card.style.display = "block";
         }
     }
 }
@@ -63,7 +74,7 @@ function selectCard() {
             let coords = card1.id.split("-"); //"0-1" -> ["0", "1"]
             let r = parseInt(coords[0]);
             let c = parseInt(coords[1]);
-            card1.src = "/img/memoryGame/" + board[r][c] + ".jpg";
+            card1.src = "/img/memoryGame/" + board[r][c] + ".png";
         }
         else if (!card2 && this !== card1) {
             card2 = this;
@@ -71,8 +82,7 @@ function selectCard() {
             let coords = card2.id.split("-"); //"0-1" -> ["0", "1"]
             let r = parseInt(coords[0]);
             let c = parseInt(coords[1]);
-
-            card2.src = "/img/memoryGame/" + board[r][c] + ".jpg";
+            card2.src = "/img/memoryGame/" + board[r][c] + ".png";
             setTimeout(update, 1000);
         }
     }
@@ -82,7 +92,7 @@ function update() {
     if (card1.src !== card2.src) {
         card1.src = "/img/memoryGame/back.png";
         card2.src = "/img/memoryGame/back.png";
-        document.getElementById("errors").innerText++;
+        errors.innerText++;
     }
     card1 = null;
     card2 = null;
